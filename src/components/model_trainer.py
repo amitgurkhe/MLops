f211d34a126000d76062feca3dcffd3024c585de
Amit Gurkhe <amitg1993@gmail.com>
2023-09-09 19:47:01 +0530

    Modelgit add .

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -18,7 +18,7 @@ from sklearn.neighbors import KNeighborsRegressor
 
 from src.exception import CustomException
 from src.logger import logging
-from src.utils import save_object
+from src.utils import save_object,evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
@@ -28,7 +28,7 @@ class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
 
-    def initiate_model_trainer(self,train_arr,test_arr,preprocessor_path):
+    def initiate_model_trainer(self,train_arr,test_arr):
         try:
             logging.info("Splitting data into train and test")
             X_train, y_train, X_test, y_test = (
@@ -48,11 +48,27 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
 
-            model_report:dict = evaluate_models(X_train=X_train, y_train=y_train, x_test= X_test, y_test = y_test, models=models)
+            model_report:dict = evaluate_models(X_train=X_train, y_train=y_train, X_test= X_test, y_test = y_test, models=models)
             
+            best_model_score = max(sorted(model_report.values()))
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model= models[best_model_name]
+            if best_model_score < 0.6 :
+                raise CustomException("No best model")
+            
+            logging.info("Best model on training and testing dataset")
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj= best_model
+            )
+            predicted = best_model.predict(X_test)
 
-        except:
-            pass
+            R2_score = r2_score(y_test,predicted)
+            return R2_score
+        except Exception as e:
+            raise CustomException(e,sys)
 
 
 
