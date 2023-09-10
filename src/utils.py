d09bfed75349f93bb665c85d7368ab33557d3afb
Amit Gurkhe <amitg1993@gmail.com>
2023-09-10 08:45:46 +0530

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import sys
 
 import numpy as np 
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split,GridSearchCV
 from sklearn.metrics import r2_score
 #import dill
 import pickle
@@ -22,12 +22,18 @@ def save_object(file_path, obj):
         raise CustomException(e,sys)
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models,param):
     try:
         #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state= 42)
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
